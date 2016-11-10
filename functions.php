1e7d44e6a2a2c0dedecc23472657d4a118e9fefa
Kert Aavik <kerta@greeny.cs.tlu.ee>
2016-11-10 13:34:36 +0200

    bootstrap

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php 
 	// functions.php
-	require("../../../config.php");
+	require("/home/kerta/config.php");
 	
 	// et saab kasutada $_SESSION muutujaid
 	// kõigis failides mis on selle failiga seotud
